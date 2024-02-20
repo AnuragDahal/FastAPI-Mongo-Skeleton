f1daa0800725814e9ea6dd8b8c4feaea43f44afa
AnuragDahal <freefirepros2020@gmail.com>
2024-02-20 20:28:24 +0545

    Add CREATE_USER function to insert a new user record

--- handlers/userhandler.py
+++ handlers/userhandler.py
@@ -1,9 +1,17 @@
+from models import schemas
 
 
-async def CREATE_USER():
-    pass
+async def CREATE_USER(request: schemas.User):
+    """
+    Insert a new student record.
+    A unique `id` will be created and provided in the response.
+    """
+    new_user = await request.insert_one(
+        request.dict(by_alias=True, exclude={"id"})
+    )
+    return new_user
 
 
 async def READ_USER():
-    
-        return {"message":"User data returned successfully."}
+
+    return {"message": "User data returned successfully."}
