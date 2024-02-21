4af84771e82778c5520b8dad9a34b940c14081ed
AnuragDahal <freefirepros2020@gmail.com>
2024-02-21 18:01:13 +0545

    Add exception handlers for different HTTP status codes

---
+++ handlers/exception.py
@@ -0,0 +1,20 @@
+from fastapi import HTTPException
+
+
+def ErrorHandler(e):
+    raise HTTPException(status_code=400, detail=str(e))
+
+def NotFoundHandler(e):
+    raise HTTPException(status_code=404, detail=str(e))
+
+def UnauthorizedHandler(e):
+    raise HTTPException(status_code=401, detail=str(e))
+
+def ForbiddenHandler(e):
+    raise HTTPException(status_code=403, detail=str(e))
+
+def ServerErrorHandler(e):
+    raise HTTPException(status_code=500, detail=str(e))
+
+def SuccessHandler(e):
+    raise HTTPException(status_code=200, detail=str(e))
