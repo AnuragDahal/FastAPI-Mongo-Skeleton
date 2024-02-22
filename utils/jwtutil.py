ca88752db977cee7af25917ca435431967241a1f
AnuragDahal <freefirepros2020@gmail.com>
2024-02-23 00:09:34 +0545

    added dependencies

--- utils/jwtutil.py
+++ utils/jwtutil.py
@@ -27,35 +27,3 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=ALGORITHM)
     return encoded_jwt
 
-
-async def verify_token(req: Request, res: Response):
-    try:
-        token = await req.cookies.get("token")
-
-        if token is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Could not validate credentials: Token missing",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-
-        payload = jwt.decode(token, secret_key, algorithms=[ALGORITHM])
-        email: str = payload.get("sub")
-
-        if email is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Could not validate credentials: 'sub' claim missing",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-
-        token_data = schemas.TokenData(email=email)
-        # req.state.token_data = token_data  # Store the token_data in the request state
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-
-    return res
