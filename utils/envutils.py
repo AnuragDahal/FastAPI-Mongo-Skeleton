3e39ee924e33abd1827931d272a706e1316ff0eb
AnuragDahal <freefirepros2020@gmail.com>
2024-02-23 21:32:56 +0545

    implemented login and logout successfully

--- utils/envutils.py
+++ utils/envutils.py
@@ -9,7 +9,7 @@ class Environment():
         self.MONGO_URI = os.getenv("MONGO_URI")
         self.secret_key = os.getenv("SECRET_KEY")
         self.algorithm = os.getenv("ALGORITHM")
-        self.access_token_expire_minutes = os.getenv(
-            "ACCESS_TOKEN_EXPIRE_MINUTES")
+        self.access_token_expire_minutes = int(os.getenv(
+            "ACCESS_TOKEN_EXPIRE_MINUTES"))
         self.TOKEN_TYPE = "bearer"
         self.TOKEN_KEY = "token"
